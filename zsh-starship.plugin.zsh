3dea0f47fe5ed6c4dfecb5df3787ffb295f6553f
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-01-08 17:50:58 +1100

    feat: only change the theme if STARSHIP_CONFIG not populated

--- zsh-starship.plugin.zsh
+++ zsh-starship.plugin.zsh
@@ -10,8 +10,11 @@ fi
 # Init cache directory for `starship` command
 local INIT_CACHE_DIR="${0:A:h}/init"
 
-# Change the default location of the Starship Config File
-export STARSHIP_CONFIG="${0:A:h}/theme/starship.toml"
+# If the 'STARSHIP_CONFIG' environment variable is not populated then change
+# the defqult theme configuration to the one provided in this plugin
+if [[ -z "$STARSHIP_CONFIG" ]]; then
+    export STARSHIP_CONFIG="${0:A:h}/theme/starship.toml"
+fi
 
 # Only regenerate init script if older than 24 hours, or does not exist
 if [[ ! -f "$INIT_CACHE_DIR/_starship"  ||  ! $(find "$INIT_CACHE_DIR/_starship" -newermt "24 hours ago" -print) ]]; then
